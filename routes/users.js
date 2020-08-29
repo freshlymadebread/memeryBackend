7fda692e8182d61614b9677b2443a0cff5f9ad22
fulinliu <fulinliu@tencent.com>
2020-08-29 16:58:46 +0800

    sercet页面

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,26 @@ let data= {}
   returnMessage(ctx,data)
 })
 
+router.get('/sercet', async (ctx, next) => {
+    //   console.log(ctx)
+    let name = ctx.request.query.name
+    let data= {}
+    let array = await userService.findUserData();
+    data={
+        ...array[0]
+    }
+    let password =  data.password
+    if(name.indexOf(password)!==-1){
+        returnMessage(ctx,{
+            name: name.split(password)[0]
+        },false)
+    }
+    else{
+        returnMessage(ctx,{
+            name: name.split(password)[0]
+        },true)
+    }
+})
 // 增加用户(POST请求)
 router.post('/add', async (ctx, next) => {
   let arr = [];
